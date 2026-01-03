df07508759d58193114b48bd5e7dc211f22eac43
Amaury <amaurymperez22@gmail.com>
2026-01-02 21:09:58 -0500

    Css incluido

--- taskFlow/settings.py
+++ taskFlow/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jjlo*i%9=cxb@6l7%(#lria^7n+14cd%^_z3krdvbo55nr*od1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'amaury.pythonanywhere.com', 
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
